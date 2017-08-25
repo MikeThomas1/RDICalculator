acc427f2c0998ceb7a5007206b29edf663f36000
Michael Thomas <mike_thomas1@me.com>
2017-08-25 17:13:35 -0500

    Created project

---
+++ RDICalculator/RDICalculator.h
@@ -0,0 +1,19 @@
+//
+//  RDICalculator.h
+//  RDICalculator
+//
+//  Created by Michael Thomas on 8/25/17.
+//  Copyright © 2017 Vault-Tec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RDICalculator.
+FOUNDATION_EXPORT double RDICalculatorVersionNumber;
+
+//! Project version string for RDICalculator.
+FOUNDATION_EXPORT const unsigned char RDICalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RDICalculator/PublicHeader.h>
+
+
