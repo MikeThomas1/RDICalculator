0fde41c39b5b5ddc7784633c40a1726b54221956
Michael Thomas <mike_thomas1@me.com>
2017-08-25 18:35:13 -0500

    Created UnitConvertible type

---
+++ RDICalculator/Public/UnitConvertible.swift
@@ -0,0 +1,49 @@
+//
+//  UnitConvertible.swift
+//  RDICalculator
+//
+//  Created by Michael Thomas on 8/25/17.
+//  Copyright © 2017 Vault-Tec. All rights reserved.
+//
+
+import Foundation
+
+
+public protocol UnitConvertible {
+    associatedtype Imperial: Unit
+    associatedtype Metric: Unit
+    
+    init(with unitOfMessure: UnitOfMeasure<Imperial, Metric>)
+}
+
+public struct Weight: UnitConvertible {
+    public typealias Imperial = Pound
+    public typealias Metric = Kilogram
+    
+    public init(with unitOfMessure: UnitOfMeasure<Imperial, Metric>) {
+        self.unitOfMessure = unitOfMessure
+    }
+    
+    private let unitOfMessure: UnitOfMeasure<Imperial, Metric>
+    
+    public var lbs: Pound {
+        switch unitOfMessure {
+        case .imperial(let lb):
+            return lb
+        case .metric(let kg):
+            let convertedValue = kg.value * 0.45359237
+            return Pound(value: convertedValue)
+        }
+    }
+    
+    public var kgs: Kilogram {
+        switch unitOfMessure {
+        case .imperial(let lb):
+            let convertedValue = lb.value * 0.45359237
+            return Kilogram(value: convertedValue)
+        case .metric(let kg):
+            return kg
+        }
+    }
+}
+
